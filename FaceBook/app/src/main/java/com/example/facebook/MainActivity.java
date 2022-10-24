57f98071afafd708181620064bd3eb0be55c77ef
sy_0420e <sy_0420e@kakao.com>
2022-10-24 17:55:41 +0900

    22.10.24

--- FaceBook/app/src/main/java/com/example/facebook/MainActivity.java
+++ FaceBook/app/src/main/java/com/example/facebook/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.example.facebook.first.FirstFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import com.google.android.material.tabs.TabLayout;
@@ -20,37 +21,34 @@ import java.util.zip.Inflater;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView btn_nav;
-    ActionBar bar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bar = getSupportActionBar();
-        btn_nav = findViewById(R.id.btn_nav);
+        btn_nav = findViewById(R.id.btm_menu);
+        FirstFragment fragment = new FirstFragment();
 
         btn_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
                 if(item.getItemId() == R.id.first_tab){
-                    bar.setTitle("홈");
+                   getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayout,new FirstFragment()).commit();
                 }else if(item.getItemId() == R.id.second_tab){
-                    bar.setTitle("Watch");
+/*
                 }else if(item.getItemId() == R.id.third_tab){
-                    bar.setTitle("프로필");
-                }else if(item.getItemId() == R.id.fourth_tab){
-                    bar.setTitle("피드");
-                }else if(item.getItemId() == R.id.five_tab){
-                    bar.setTitle("알림");
-                }else if(item.getItemId() == R.id.six_tab){
-                    bar.setTitle("메뉴");
-                }
 
+                }else if(item.getItemId() == R.id.fourth_tab){
+
+                }else if(item.getItemId() == R.id.five_tab){
+
+                }else if(item.getItemId() == R.id.six_tab){
+                    */
+                }
                 return false;
             }
         });
-
     }
 }
